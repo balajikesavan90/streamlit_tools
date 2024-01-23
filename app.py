0ff042812970401a76b195db5970b6f61504c9a1
BalajiKesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-01-22 22:15:59 -0800

    Update page title and app title

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from widgets.merge_pdfs import render_merge_pdfs_widget
 
 # Set the page configuration
 st.set_page_config(
-    page_title='Free PDFs Tools',  # The title of the page
+    page_title='Free PDF Merger',  # The title of the page
     page_icon=':page_facing_up:'  # The icon of the page
 )
 
 # Set the title of the page
-st.title(':blue[Free PDF Tools]')
+st.title(':blue[Free PDF Merger]')
 
 if 'pdf_merger' not in st.session_state:
     st.session_state['pdf_merger'] = {}

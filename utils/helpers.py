b506523960227a308b94e3aae131164c35198f30
BalajiKesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-02-25 19:50:51 -0800

    Refactor file merging logic in helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,30 +1,32 @@
 import streamlit as st
-import PyPDF2
 from io import BytesIO
 import img2pdf
+import fitz
 
 def merge_files(session_page):
     with st.spinner('Merging files...'):
-        pdf_writer = PyPDF2.PdfWriter()
+        merged_pdf = fitz.open()  # create a new empty PDF
 
         for file in session_page['input_file_list']:
             if file.name.endswith('.pdf') or file.name.endswith('.PDF'):
-                pdf_reader = PyPDF2.PdfReader(file)
-                if pdf_reader.is_encrypted:
+                doc = fitz.open("pdf", BytesIO(file.getvalue()))
+                if doc.needs_pass:
                     st.error("The file is password protected. Please upload a decrypted file.")
                     return
-                for page in range(len(pdf_reader.pages)):
-                    pdf_writer.add_page(pdf_reader.pages[page])
+                else:
+                    for page in doc:
+                        merged_pdf.insert_pdf(doc, from_page=page.number, to_page=page.number)
             else:
                 image_pdf = img2pdf.convert(file.read())
                 image_pdf = BytesIO(image_pdf)
-                pdf_writer.append(PyPDF2.PdfReader(image_pdf))
+                image_doc = fitz.open("pdf", image_pdf.read())
+                merged_pdf.insert_pdf(image_doc)
 
-        merged_pdf = BytesIO()
-        pdf_writer.write(merged_pdf)
-        merged_pdf.seek(0)
+        output = BytesIO()
+        merged_pdf.save(output, garbage=4, deflate=True)
+        output.seek(0)
 
-    session_page['merged_pdf'] = merged_pdf
+    session_page['merged_pdf'] = output
     st.session_state['pdf_merger'] = session_page
 
 def reset(session_page, page):
